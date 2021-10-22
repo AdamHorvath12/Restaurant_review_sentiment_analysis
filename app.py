482c6d45ee6b7b0aba44aa326a74ae6f391d29b8
AdamHorvath12 <horvath.reparszky.a@gmail.com>
2021-10-22 15:09:08 +0200

    Debug app.py

--- app.py
+++ app.py
@@ -1,35 +1,28 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
+from flask.globals import request
 import pickle
-
-
 filename = 'restaurant-sentiment-mnb-model.pkl'
 classifier = pickle.load(open(filename, 'rb'))
-cv = pickle.load(open('cv-transform.pkl','rb'))
+cv = pickle.load(open('cv-transform.pkl', 'rb'))
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-	return render_template('index.html')
-
-""" @app.route('/predict', methods=['POST'])
-def predict():
-    if request.method == 'POST':
-    	message = request.form['message']
-    	data = [message]
-    	vect = cv.transform(data).toarray()
-    	my_prediction = classifier.predict(vect)
-    	return render_template('result.html', prediction=my_prediction) """
-
+    return render_template('index.html')
 @app.route('/predict', methods=['POST'])
 def predict():
     if request.method == 'POST':
         message = request.form.get('message')
         if not message:
-			my_prediction = classifier.predict(cv.transform([message]).toarry())
+            data = [message]
+            vect = cv.transform(data).toarray()
+            my_prediction = classifier.predict(vect)
         else:
-			my_prediction = -1
-	return render_template('result.html', prediction=my_prediction)
+            my_prediction = -1
+        return render_template('result.html', prediction=my_prediction)
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
+
+
