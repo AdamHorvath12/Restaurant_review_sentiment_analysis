6d5701fb641e44087260c4c464d38b3126badb42
AdamHorvath12 <horvath.reparszky.a@gmail.com>
2021-10-22 14:56:11 +0200

    Debug

--- app.py
+++ app.py
@@ -12,14 +12,24 @@ app = Flask(__name__)
 def home():
 	return render_template('index.html')
 
-@app.route('/predict', methods=['POST'])
+""" @app.route('/predict', methods=['POST'])
 def predict():
     if request.method == 'POST':
     	message = request.form['message']
     	data = [message]
     	vect = cv.transform(data).toarray()
     	my_prediction = classifier.predict(vect)
-    	return render_template('result.html', prediction=my_prediction)
+    	return render_template('result.html', prediction=my_prediction) """
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        message = request.form.get('message')
+        if not message:
+			my_prediction = classifier.predict(cv.transform([message]).toarry())
+        else:
+			my_prediction = -1
+	return render_template('result.html', prediction=my_prediction)
 
 if __name__ == '__main__':
 	app.run(debug=True)
